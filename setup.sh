879ba4fe4affc9b8021ec45cdcbd5088e608e813
Phil Trinh <ptrinh@me.com>
2022-08-14 21:23:55 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,13 +12,13 @@ brew install git php ruby node npm protobuf redis mysql rabbitmq mercurial wget 
 
 # apps
 echo Installing desktop apps...
-brew install --cask google-chrome iterm2 folx dropbox adobe-acrobat-reader ledger-live textmate skype slack firefox vlc zoom viber microsoft-edge openemu anydesk microsoft-office steam time-out
+brew install --cask google-chrome iterm2 discord spotify folx dropbox adobe-acrobat-reader ledger-live textmate skype slack firefox vlc zoom viber microsoft-edge openemu anydesk microsoft-office steam time-out
 # utils
 echo Installing utilities...
-brew install --cask little-snitch istat-menus clipy grandperspective ipartition rectangle vipriser cd-to blackhole-2ch osxfuse handbrake the-unarchiver textbar adguard android-file-transfer balenaetcher sidequest openkey switchresx
+brew install --cask little-snitch istat-menus clipy transmit microsoft-remote-desktop app-tamer grandperspective ipartition rectangle vipriser cd-to blackhole-2ch macfuse handbrake the-unarchiver textbar android-file-transfer balenaetcher sidequest openkey switchresx
 # dev tools
 echo Installing dev tools...
-brew install --cask oracle-jdk docker sequel-ace visual-studio-code goland phpstorm tunnelblick dosbox-x postman wireshark altserver ifunbox sourcetree proxyman android-platform-tools zsh-syntax-highlighting docker
+brew install --cask oracle-jdk docker sequel-ace charles fork visual-studio-code goland phpstorm tunnelblick dosbox-x postman wireshark altserver ifunbox sourcetree android-platform-tools docker
 
 # default-writes
 echo Setting default-writes...
